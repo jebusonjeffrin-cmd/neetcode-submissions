51bf0ee40776c4d06e0019a6808748abe3c52196
JEFFRIN JEBUSON K <jebusonjeffrin@gmail.com>
2026-08-11 17:36:31 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mn=prices[0],ans=0;
+        for(int i=1;i<prices.length;i++){
+            ans = Math.max(ans,prices[i]-mn);
+            mn = Math.min(mn,prices[i]);
+        }
+        return ans;
+    }
+}
