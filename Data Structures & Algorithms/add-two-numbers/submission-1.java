e9add3776068e4897592488720b453d0ca43409b
JEFFRIN JEBUSON K <jebusonjeffrin@gmail.com>
2026-09-05 22:53:39 +0530

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode ans = result;
+        ListNode t1 = l1,t2 = l2;
+        int cf=0;
+        while(t1 != null || t2 != null || cf != 0){
+            int v1 = (t1 != null)?t1.val:0;
+            int v2 = (t2 != null)?t2.val:0;
+
+            int res = v1 + v2 + cf;
+            cf = res / 10;
+            res = res % 10;
+            ans.next = new ListNode(res);
+            ans = ans.next;
+            t1 = (t1 != null)?t1.next:null;
+            t2 = (t2 != null)?t2.next:null; 
+        }
+        return result.next;
+    }
+}
